10d54517fdbd47a61d7d5d58f8ee3c7a7769ac4f
aronsommer <aronsommer@gmail.com>
2022-10-25 16:49:06 +0200

    remove henlo popup

--- map-extras.js
+++ map-extras.js
@@ -3,12 +3,12 @@ import { map } from './map.js';
 export function addExtrasToMap() {
 
     // A standalone popup
-    var popup = new L.popup({
-        closeOnClick: false,
-        autoClose: false
-    }).setLatLng([52, 8.2275])
-        .setContent("Henlo :-)")
-        .addTo(map);
+    // var popup = new L.popup({
+    //     closeOnClick: false,
+    //     autoClose: false
+    // }).setLatLng([52, 8.2275])
+    //     .setContent("Henlo :-)")
+    //     .addTo(map);
 
     // A marker
     // var marker = L.marker([55.5, -0.09]).addTo(map);
